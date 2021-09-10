f120026bb1da0de827d738f8a5fb0320b6dc6a16
Anish-Nirala <anish.santim@gmail.com>
2021-09-10 11:43:18 +0530

    Initial Commit

---
+++ res.py
@@ -0,0 +1,4 @@
+consumerKey = 'nAfAiljzt8Ntd1oatjYU2uvKc'
+consumerSecret = 'JBJahC7nKS4q853V0vodZ8buaYGHwySv6u5XFm52dlownOqTRm'
+accessToken = '4880646753-8TzBk1UEjuvXEXR2XL7uJ2ZFMRZnwNrDOioUHI9'
+accessTokenSecret = 'CjAas3cB7A9OW7SJFA6SOkyfgv61MyN59W0g47ZSqBL2b'
